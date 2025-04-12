fdc3a57567956246c5ad256094f32dc539e3fc92
Umang Gupta <umanggupta@Umangs-MacBook-Pro.local>
2025-04-12 12:38:45 +0530

    Initial Commit

---
+++ src/main/java/com/umang/arpitBhayaniClasses/ArpitBhayaniClassesApplication.java
@@ -0,0 +1,13 @@
+package com.umang.arpitBhayaniClasses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArpitBhayaniClassesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArpitBhayaniClassesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/umang/arpitBhayaniClasses/ArpitBhayaniClassesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umang.arpitBhayaniClasses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArpitBhayaniClassesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
