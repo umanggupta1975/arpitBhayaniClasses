4602fe55041d251743e56c02df234b5784be849a
umanggupta <umanggupta1975@gmail.com>
2025-05-06 22:54:25 +0530

    Implement Kafka

---
+++ src/main/java/com/system/design/Week1/kafka/MyKafkaConsumer1.java
@@ -0,0 +1,39 @@
+package com.system.design.Week1.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class MyKafkaConsumer1 {
+    public static void main(String[] args) {
+        String bootstrapServers = "localhost:9092";
+        String topic = "my-topic";
+
+        // Create consumer properties
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "my-group");
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+
+        // Create a Kafka consumer
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+
+        // Subscribe to the topic
+        consumer.subscribe(Collections.singletonList(topic));
+
+        // Poll for new messages
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+            for (ConsumerRecord<String, String> record : records) {
+                System.out.println("Received: " + record.value());
+            }
+        }
+    }
+}
